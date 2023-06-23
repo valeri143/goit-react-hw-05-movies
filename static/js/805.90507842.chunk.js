aeb523dafea48edde69b3729cdd2193fcc09b291
valeri143 <117216733+valeri143@users.noreply.github.com>
2023-06-23 18:19:21 +0000

    Deploying to gh-pages from @ valeri143/goit-react-hw-05-movies@bb4d0fdefeb49bfcc949d46527acd92c67a168d4 🚀

---
+++ static/js/805.90507842.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,r,t){t.r(r);var n=t(184);r.default=function(){return(0,n.jsx)("h1",{children:"Sorry something was wrong"})}}}]);
+//# sourceMappingURL=805.90507842.chunk.js.map
